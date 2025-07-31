4c29ea35768038fde9bdd4f74604c21281360558
BorhanBappy <bappyzx1@gmail.com>
2025-07-31 17:05:18 +0600

    PermissionPolicy make

---
+++ src/Application/WebApi/Permissions/PermissionAuthenticationHandler.cs
@@ -0,0 +1,30 @@
+using Common.Authorization;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApi.Permissions;
+
+public class PermissionAuthenticationHandler : AuthorizationHandler<PermissionRequirement>
+
+{
+    public PermissionAuthenticationHandler()
+    {
+    }
+
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
+        PermissionRequirement requirement)
+    {
+        if (context.User is null)
+        {
+            await Task.CompletedTask;
+        }
+
+        var permissions = context.User.Claims.Where(claim =>
+            claim.Type == AppClaim.Permission && claim.Value == requirement.Permission &&
+            claim.Issuer == "LOCAL AUTHORITY");
+        if (permissions.Any())
+        {
+            context.Succeed(requirement);
+            await Task.CompletedTask;
+        }
+    }
+}
